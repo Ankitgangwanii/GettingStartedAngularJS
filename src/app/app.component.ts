0e4d4c2ae49914edf34d5ffaf132cdfc14fa8d90
Ankit Gangwani <Ankitgangwani160@gmail.com>
2025-03-06 15:38:04 +0530

    [Ankit] Add . Implemented Event Binding to open BridgeLabz website on logo click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'] 
 })
@@ -11,9 +10,13 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "assets/Logo.jpg"; // Ensure logo.jpg is in src/assets/
-
+  url = "https://www.bridgelabz.com";
 
   ngOnInit():void{
     this.title = "Hello from BridgeLabz,";
+  }
+  onClick(event: Event){
+    console.log("Save button is clicked", event);
+    window.open(this.url,"_blank");
   }
 }
