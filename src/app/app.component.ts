d43c08ebe84f3fa3226ae9c210857635542f052d
Ankit Gangwani <Ankitgangwani160@gmail.com>
2025-03-06 22:04:00 +0530

    [Ankit] Add . Added name validation with input event binding in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,14 @@ import { RouterModule } from '@angular/router';
   standalone: true,  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [FormsModule, RouterModule] 
+  imports: [FormsModule, RouterModule]  // ✅ Ensure FormsModule is imported
 })
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "assets/Logo.jpg";
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = '';
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
@@ -22,5 +23,18 @@ export class AppComponent {
   onClick(event: Event): void {
     console.log("Save button is clicked", event);
     window.open(this.url, "_blank");
+  }
+
+  
+  onInput(event: Event): void {
+    this.userName = (event.target as HTMLInputElement).value; 
+
+    console.log('Change Event Occurred!', this.userName);
+    const nameRegex = /^[A-Z][a-zA-Z\s]{2,}$/; 
+    if (nameRegex.test(this.userName)) {
+      this.nameError = ''; 
+    } else {
+      this.nameError = 'Name is Incorrect!';
+    }
   }
 }
